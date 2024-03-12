ffa178b61249f883284c15096af891c804391bb5
niclasm17 <87148097+niclasm17@users.noreply.github.com>
2024-03-12 19:30:12 +0100

    Update certbot-hetzner-auth.py
    Bugfix for subdomains

--- certbot-hetzner-auth.py
+++ certbot-hetzner-auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import json, requests, sys, getopt, argparse, time
+import json, requests, sys, getopt, argparse, time, re
 from os import environ
 
 f = open("hetzner-auth-token", "r")
@@ -8,6 +8,10 @@ api_token = f.read() # your Hetzner DNS-API Token
 f.close()
 headers = {'Auth-API-Token': api_token, 'Content-Type': 'application/json'}
 response = requests.get('https://dns.hetzner.com/api/v1/zones', headers=headers)
-zone_id = [x for x in json.loads(response.content.decode('utf-8'))['zones'] if x['name'] == environ['CERTBOT_DOMAIN']][0]['id']
-response = requests.post('https://dns.hetzner.com/api/v1/records', headers=headers, json={'value': environ['CERTBOT_VALIDATION'], 'ttl': 86400, 'type': 'TXT', 'name': '_acme-challenge', 'zone_id': zone_id})
+
+domain = re.search(r'.*\.(.*\..*)', environ['CERTBOT_DOMAIN']).group(1)
+subdomains = '.' + re.match(r'^(.*\.)?[^.]*\.[^.]*$', environ['CERTBOT_DOMAIN']).group(1)[:-1] if re.match(r'^(.*\.)?[^.]*\.[^.]*$', environ['CERTBOT_DOMAIN']) and re.match(r'^(.*\.)?[^.]*\.[^.]*$', environ['CERTBOT_DOMAIN']).group(1) else ''
+
+zone_id = [x for x in json.loads(response.content.decode('utf-8'))['zones'] if x['name'] == domain][0]['id']
+response = requests.post('https://dns.hetzner.com/api/v1/records', headers=headers, json={'value': environ['CERTBOT_VALIDATION'], 'ttl': 86400, 'type': 'TXT', 'name': '_acme-challenge' + subdomains, 'zone_id': zone_id})
 time.sleep(30)
